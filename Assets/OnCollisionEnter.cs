1dc0738d5fe182b15658af28b4ea3371bdf6dcfd
Michael897922 <mishaionov2013@gmail.com>
2026-04-02 19:07:58 +0700

    [[[[[

---
+++ Assets/OnCollisionEnter.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OnCollisionEnter : MonoBehaviour
+
+
+
+
+
+{
+    
+    [SerializeField] Text ochitext;
+
+    int ochi = 0;
+    private void OnTriggerEnter(Collider other)
+ {
+    ochi ++;
+    ochitext.text = "собрано :  " + ochi;
+     SceneManager.LoadScene("DungeonLiteScene-FPS");
+    print(ochi);
+    Destroy(gameObject);
+    
+
+ }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
